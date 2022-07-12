daad9f2ee9d7b93ee5d386d8a50981097b3b999e
Arsatius Regler <arsatius@uber.space>
2022-07-12 09:44:14 +0200

    update project

--- Components/Package.swift
+++ Components/Package.swift
@@ -4,37 +4,30 @@
 import PackageDescription
 
 let package = Package(
-    name: "Components",
-		defaultLocalization: "en",
-		platforms: [.iOS(.v15)],
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library( name: "Components", targets: ["Components"]),
-				.library(name: "AssetKit", targets: ["AssetKit"]),
-				.library(name: "ViewComponents", targets: ["ViewComponents"])
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "Components",
-            dependencies: ["AssetKit"]),
-				.target(
-					name: "AssetKit",
-					plugins: ["SwiftGenPlugin"]
-				),
-				.target(
-					name: "ViewComponents",
-					dependencies: ["AssetKit"]
-				),
-        .testTarget(
-            name: "ComponentsTests",
-            dependencies: ["Components"]),
-    ]
+	name: "Components",
+	defaultLocalization: "en",
+	platforms: [.iOS(.v15)],
+	products: [
+		// Products define the executables and libraries a package produces, and make them visible to other packages.
+		.library( name: "Components", targets: ["Components"]),
+		.library(name: "AssetKit", targets: ["AssetKit"]),
+	],
+	dependencies: [
+		// Dependencies declare other packages that this package depends on.
+		// .package(url: /* package url */, from: "1.0.0"),
+	],
+	targets: [
+		// Targets are the basic building blocks of a package. A target can define a module or a test suite.
+		// Targets can depend on other targets in this package, and on products in packages this package depends on.
+		.target(
+			name: "Components",
+			dependencies: ["AssetKit"]),
+		.target(
+			name: "AssetKit",
+			resources: [.process("swiftgen.yml")],
+			plugins: ["SwiftGenPlugin"]
+		)
+	]
 )
 
 // MARK: Plugins
--- Components/Plugins/SwiftGenPlugin/SwiftGenPlugin.swift
+++ Components/Plugins/SwiftGenPlugin/SwiftGenPlugin.swift
@@ -3,33 +3,33 @@ import PackagePlugin
 
 @main
 struct SwiftGenPlugin: BuildToolPlugin {
+	private let swiftGenConfigFilename = "swiftgen.yml"
 
-    func createBuildCommands(context: PluginContext, target: Target) throws -> [Command] {
-        // This example configures `swiftgen` to take inputs from a `swiftgen.yml` file.
-      let swiftGenConfigFile = context.package.directory.appending("swiftgen.yml")
+	func createBuildCommands(context: PluginContext, target: Target) throws -> [Command] {
+		// This example configures `swiftgen` to take inputs from a `swiftgen.yml` file.
+		let swiftGenConfigFile = target.directory.appending(swiftGenConfigFilename)
+		// This example configures `swiftgen` to write to a "SwiftGenOutputs" directory.
+		let swiftGenOutputsDir = context.pluginWorkDirectory.appending("SwiftGenOutputs")
+		try FileManager.default.createDirectory(atPath: swiftGenOutputsDir.string, withIntermediateDirectories: true)
 
-        // This example configures `swiftgen` to write to a "SwiftGenOutputs" directory.
-        let swiftGenOutputsDir = context.pluginWorkDirectory.appending("SwiftGenOutputs")
-        try FileManager.default.createDirectory(atPath: swiftGenOutputsDir.string, withIntermediateDirectories: true)
-
-        // Return a command to run `swiftgen` as a prebuild command. It will be run before
-        // every build and generates source files into an output directory provided by the
-        // build context.
-        return [.prebuildCommand(
-            displayName: "Running SwiftGen",
-            executable: try context.tool(named: "swiftgen").path,
-            arguments: [
-                "config",
-                "run",
-                "--verbose",
-                "--config", "\(swiftGenConfigFile)"
-            ],
-            environment: [
-                "PROJECT_DIR": "\(context.package.directory)",
-                "TARGET_NAME": "\(target.name)",
-                "DERIVED_SOURCES_DIR": "\(swiftGenOutputsDir)"
-            ],
-            outputFilesDirectory: swiftGenOutputsDir)
-        ]
-    }
+		// Return a command to run `swiftgen` as a prebuild command. It will be run before
+		// every build and generates source files into an output directory provided by the
+		// build context.
+		return [.prebuildCommand(
+			displayName: "Running SwiftGen",
+			executable: try context.tool(named: "swiftgen").path,
+			arguments: [
+				"config",
+				"run",
+				"--verbose",
+				"--config", "\(swiftGenConfigFile)"
+			],
+			environment: [
+				"PROJECT_DIR": "\(context.package.directory)",
+				"TARGET_NAME": "\(target.name)",
+				"DERIVED_SOURCES_DIR": "\(swiftGenOutputsDir)"
+			],
+			outputFilesDirectory: swiftGenOutputsDir)
+		]
+	}
 }
